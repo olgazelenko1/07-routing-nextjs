3d3bc5aed484bd26ef80b09d21b8974103d4cf28
OLGA ZELENKO <olichkazelenko@gmail.com>
2025-08-15 13:55:45 +0100

    fix-bag

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,63 +1,29 @@
-"use client";
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from '@tanstack/react-query';
+import { fetchNoteById } from '@/lib/api';
+import NotePreview from './NotePreview.client';
 
-import { useParams, useRouter } from "next/navigation";
-import { useQuery } from "@tanstack/react-query";
-import { fetchNoteById } from "@/lib/api";
-import Modal from "@/components/Modal/Modal";
-import css from "./NotePreview.module.css";
+type Props = {
+  params: Promise<{ id: string }>;
+};
 
-export default function ModalNotePage() {
-  const router = useRouter();
-  const { id } = useParams<{ id: string }>();
+const NoteDetails = async ({ params }: Props) => {
+  const { id } = await params;
+  const queryClient = new QueryClient();
 
-  const {
-    data: note,
-    isLoading,
-    error,
-  } = useQuery({
-    queryKey: ["note", id],
-    queryFn: () => fetchNoteById(id!),
-    enabled: !!id,
+  await queryClient.prefetchQuery({
+    queryKey: ['note', id],
+    queryFn: () => fetchNoteById(id),
   });
 
-  if (isLoading) {
-    return (
-      <Modal onClose={() => router.back()}>
-        <div className={css.container}>
-          <p>Loading...</p>
-        </div>
-      </Modal>
-    );
-  }
-
-  if (error || !note) {
-    return (
-      <Modal onClose={() => router.back()}>
-        <div className={css.container}>
-          <p>Note not found</p>
-        </div>
-      </Modal>
-    );
-  }
-
   return (
-    <Modal onClose={() => router.back()}>
-      <div className={css.container}>
-        <div className={css.item}>
-          <div className={css.header}>
-            <h2>{note.title}</h2>
-          </div>
-          <p className={css.content}>{note.content}</p>
-          {note.tag && <p className={css.tag}>{note.tag}</p>}
-          <p className={css.date}>
-            {new Date(note.createdAt).toLocaleDateString("en-GB", {
-              day: "2-digit",
-              month: "2-digit",
-              year: "numeric",
-            })}
-          </p>
-        </div>
-      </div>
-    </Modal>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotePreview id={id} />
+    </HydrationBoundary>
   );
-}
+};
+
+export default NoteDetails;
